1db5ac750faa3156f4f8e11cbc94f818ab3aedb0
Omar Hidalgo <163226666+OmarHG098@users.noreply.github.com>
2024-04-08 02:12:10 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Nueva!");
+  document.querySelector("#excuse").innerHTML = excusa();
+};
+let excusa = () => {
+  let quien = ["Mi perro ", "Mi mamá ", "El vecino ", "Un alien "];
+  let que = ["mordió ", "pateó ", "destruyó ", "incendió "];
+  let objeto = ["mi tarea ", "mi laptop ", "mi cara ", "mi auto "];
+  let cuando = [
+    "antes de clases.",
+    "mientras dormía.",
+    "por la mañana.",
+    "anoche."
+  ];
+
+  let quienIndx = Math.floor(Math.random() * quien.length);
+  let queIndx = Math.floor(Math.random() * que.length);
+  let objetoIndx = Math.floor(Math.random() * objeto.length);
+  let cuandoIndx = Math.floor(Math.random() * cuando.length);
+  return (
+    quien[quienIndx] + que[queIndx] + objeto[objetoIndx] + cuando[cuandoIndx]
+  );
 };
